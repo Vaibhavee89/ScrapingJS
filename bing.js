a713e1e909c0c5d14cb8bae006baa48b664aaa43
Vaibhavee Singh <vaibhaveesingh89@gmail.com>
2023-11-24 23:04:29 +0530

    Add files via upload

---
+++ bing.js
@@ -0,0 +1,39 @@
+var casper = require('casper').create({
+    verbose: true,
+    logLevel:'error',
+    // clientScripts:[]
+});
+
+var links = [];
+
+function getLinks(){
+    var links = document.querySelectorAll('.b_algo a');
+    return Array.prototype.map.call(links, function(e){
+        return e.getAttribute('href');
+    })
+}
+
+casper.start('http://bing.com/', function(){
+    this.fill('form[action="/search]',{
+        q:'casperjs'
+    },true);
+})
+
+casper.then(function(){
+    links = this.evaluate(getLinks);
+
+// now search for 'phantomjs' by filling the form in again
+    this.fill('form[action="/search"]',{
+        q: 'phantomjs'
+    },true);
+    })
+
+casper.then(function(){
+    links = concat(this.evaluate(getLinks));
+});
+
+casper.run(function(){
+    //echo results in readable fashion
+    this.echo(links.length+'links found:');
+    this.echo('-'+links.join('\n-')).exit();
+});
